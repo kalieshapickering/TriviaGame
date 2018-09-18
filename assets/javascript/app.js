920bba924823607d7fdc84a6118c62f6915e0ac6
kalistan <kaliesha.pickering@gmail.com>
2018-09-17 21:20:41 -0400

    update render question, render button, and jquery click handlers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,109 +11,114 @@ var $secondBtn = $('.second-btn');
 var $thirdBtn = $('.third-btn');
 var $fourthBtn = $('.fourth-btn');
 var score = 0;
+var questionNumber = 0;
 
 
 
 var game = {
-    triviaQuestions: {
-        "spongeBob": {
+    triviaQuestions: [
+       {
             q: "SpongeBob SquarePants works at which fast food establishment?",
             image: "assets/images/spongebob.jpg",
             o: ["The Chum Bucket", "The Krusty Krab", "The Fast-food Coliseum", "The Goofy Goobers Icecream Party Boat"],
             a: "The Krusty Krab"
         },
-        "family guy": {
+        {
             q: "Who is Meg's real father?",
             image: "assets/images/familyguy.jpg",
             o: ["Peter Griffin", "Glenn Quagmire", "Stan Thompson", "Brian Griffin"],
             a: "Stan Thompson"
         },
-        "simpsons": {
+        {
             q: "What is the name of Bart's favorite comic book action hero?",
             image: "assets/images/simpsons.jpg",
             o: ["Radiation Dude", "Mezmorino", "Radioactive Man", "The Annihilator"],
             a: "Radioactive Man"
         },
-        "clevlend show": {
+        {
             q: "What does Cleveland do for a living?",
             image: "assets/images/cleveland.gif",
             o: ["Plumber", "Electrician", "Cable Install Man", "Mechanic"],
             a: "Cable Install Man"
         },
-        "fairly odd parents": {
+        {
             q: "Who has a crush on Timmy Turner?",
             image: "assets/images/fairyodd.jpg",
             o: ["Trixie", "Cutie", "Tootie", "Josie"],
             a: "Tootie"
         },
-        "rick and morty": {
+         {
             q: "What does Rick use to travel between dimensions and universes?",
             image: "assets/images/rickmorty.jpg",
             o: ["Space Laser", "Portal Gun", "Tardis", 'Universe Key'],
             a: "Portal Gun"
         },
-        "big mouth": {
+        {
             q: "Which character does 'Nick Kroll' not cover the voice over for?",
             image: "assets/images/bigmouth.gif",
             o: ["Nick", "Hormone Monster", "Lola", "Grandpa Andrew"],
             a: "Grandpa Andrew"
         },
-        "samurai jack": {
+       {
             q: "When was Samurai Jack Aired?",
             image: "assets/images/samuraijack.jpg",
             o: ["2004", "2001", "1998", "2000"],
             a: "2001"
         },
-        "south park": {
+        {
             q: "What was the first episode of 'South Park'?",
             image: "assets/images/southpark.jpeg",
             o: ["Volcano", "Tom's Rhinoplasty", "Mr. Hankey, The Christmas Poo", "Cartman Gets an Anal Probe"],
             a: "Cartman Gets an Anal Probe"
         },
-        "beavis and butthead": {
+         {
             q: "Where does Butt-head say he wants to live closer too?",
             image: "assets/images/beavisandbutthead.gif",
             o: ["Bar", "Theme Park", "Airport", "Highway"],
             a: "Highway"
         },
-        "dexter lab": {
+         {
             q: "What is the main secret entrance to Dextar's lab?",
             image: "assets/images/dexterlab.jpg",
             o: ["Inside the Computer", "Through the Bookshelf", "Under the Rug", "Through the Closet"],
             a: "Through the Bookshelf"
         },
-    },
+    ],
+loadQuestion: function(){
+for (var i=0; i<this.triviaQuestions[question].length; i++){
 
-    renderQuestion: function () {
-        if (i <= (this.triviaQuestions.length - 1)) {
-            for (var i = 0; i < this.triviaQuestions.length; i++) {
-                var $questionDiv = $("<div>");
-                var question = this.triviaQuestions[i].q;
+}
+},
+     renderQuestion: function () {
+    $('.question-view').empty();
+               var $questionDiv = $("<div>");
+               var question = this.triviaQuestions[questionNumber].q;
                 $questionDiv.append(question);
                 $(".question-view").append($questionDiv);
-                console.log(this.triviaQuestions[i]);
-            }
+                console.log(this.triviaQuestions[questionNumber].q);
+           //  }
             this.renderButton();
-        }
+       // }
     },
 
     //render button with answer options
     renderButton: function () {
-        var options = this.triviaQuestions[i].o;
-        for (var i = 0; i < triviaQuestions.options.length; i++) {
+        $(".options-view").empty();
+        var options = this.triviaQuestions[questionNumber].o;
+        for (var i = 0; i < this.triviaQuestions[questionNumber].o.length; i++) {
             var $button = $("<button>");
             var option = options[i];
-            $button.addClass("btn btn-info btn-lg");
-            $button.attr("data-value",)
+            $button.addClass("btn btn-info btn-lg answer");
+            $button.data("value",option)
             $button.text(option);
-            $("<options-view>").append($button);
+            $(".options-view").append($button);
         }
 
     },
 
     setUp: function () {
         this.renderQuestion();
-   
+        $playBtn.hide();
         
     },
   
@@ -124,9 +129,17 @@ var game = {
         $score.html(score);
     },
     // see if the question is correct
-    checkCorrect: function () {
-
-        this.checkWinner();
+    checkCorrect: function (data) {
+ if(data == this.triviaQuestions[questionNumber].a){
+     this.updateScore();
+     console.log("This works");
+ }
+        else{
+       console.log("WRONG");
+        } 
+      // this.checkWinner();
+      questionNumber ++;
+       this.renderQuestion();
     },
     // if you win the game
     checkWinner: function () {}
@@ -139,33 +152,15 @@ var game = {
 $(document).ready(function () {
 
     //game click handlers
-    $firstBtn.on('click', function () {
-        checkCorrect();
-        renderQuestion();
-        updateScore();
+    $(document).on('click',".answer", function () {
+        var data = $(this).data("value")
 
+        game.checkCorrect(data);
     });
 
-    $secondBtn.on('click', function () {
-        checkCorrect();
-        renderQuestion();
-        updateScore();
-    });
 
-    $thirdBtn.on('click', function () {
-        checkCorrect();
-        renderQuestion();
-        updateScore();
-    });
-    $fourthBtn.on('click', function () {
-        checkCorrect();
-        renderQuestion();
-        updateScore();
-    });
-
-    $playBtn.on('click', function () {
+    $playBtn.on('click', function(){
         game.setUp();
-        game.updateScore();
     });
-
+    
 });
